b9804946f7a1ebfe015126f16e944c136ca95037
hnbae-dev <hnbae.dev@gmail.com>
2024-07-16 16:00:28 +0900

    3-7 로그인과 로그아웃 기능 구현하기

--- sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mysite.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,7 +27,14 @@ public class SecurityConfig {
             .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
         // h2-console 페이지의 프레임 설정을 허용
         .headers((headers) -> headers
-            .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+            .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+        .formLogin((formLogin) -> formLogin
+            .loginPage("/user/login")
+            .defaultSuccessUrl("/"))
+        .logout((logout) -> logout
+            .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true));
 
         return http.build();
   }
@@ -33,5 +42,11 @@ public class SecurityConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }
